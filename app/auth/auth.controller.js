88aaaa7a899d900894fa9db52080cbea32e77e0a
Kolya <frdvmn@mail.ru>
2025-02-26 13:59:31 +0300

    add dev condition, dotenv

--- app/auth/auth.controller.js
+++ app/auth/auth.controller.js
@@ -2,5 +2,5 @@
 // @route		POST /api/auth/login
 // @access	Public
 export const authUser = (req, res) => {
-	res.send('Authenticated successfully')
+	res.send(`Authenticated successfully ${req.body.username}`)
 }
--- server.js
+++ server.js
@@ -1,14 +1,23 @@
+import 'colors'
+import dotenv from 'dotenv'
 import express from 'express'
+import morgan from 'morgan'
 import authRoutes from './app/auth/auth.routes.js'
+
+dotenv.config()
 const app = express()
 
 async function main() {
+	process.env.NODE_ENV === 'development' && app.use(morgan('dev'))
 	app.use(express.json())
 	app.use('/api/auth', authRoutes)
 
-	const PORT = 5000
+	const PORT = process.env.PORT || 5000
+
 	app.listen(PORT, () => {
-		console.log(`Server is running on ${PORT}`)
+		console.log(
+			`Server is running in ${process.env.NODE_ENV} on PORT:${PORT}`.green.bold
+		)
 	})
 }
 
