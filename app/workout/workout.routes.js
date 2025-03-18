e3b4f129098066a9af1f412a3a4492058200346b
Kolya <frdvmn@mail.ru>
2025-03-18 14:58:56 +0300

    Добавлено добавление воркаут лога

--- app/workout/workout.routes.js
+++ app/workout/workout.routes.js
@@ -7,10 +7,14 @@ import {
 	updateWorkout
 } from './workout.controller.js'
 
+import { createWorkoutLog } from './log/workout-log.controller.js'
+
 const router = express.Router()
 
 router.route('/').post(createWorkout).get(readWorkouts)
 router.route('/:id').put(updateWorkout).delete(deleteWorkout)
 router.route('/:id').get(readWorkout)
+
+router.route('/log/:id').post(createWorkoutLog)
 
 export default router
